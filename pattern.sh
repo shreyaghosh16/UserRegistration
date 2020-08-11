516074141dd595aded9195b3e3e0973011fada2f
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-11 10:15:32 +0530

    UC-4 : Validating Mobile No.

--- pattern.sh
+++ pattern.sh
@@ -31,7 +31,18 @@ pattern1="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]
 read -p "Enter Email to check " email
 if [[ $email =~ $pattern1 ]]
 then
-   echo " Email is Correct"
+   echo "Email is Correct"
 else
    echo "Email is Incorrect"
+fi
+
+#UC-4 validating Mobile no
+
+pattern2="^[0-9]{2}[' ']{0,1}[0-9]{10}$"
+read -p "Enter Mobile no to check " mobile_no
+if [[ $mobile_no =~ $pattern2 ]]
+then
+   echo " Mobile no is Correct "
+else
+   echo "Mobile no is Wrong "
 fi
