1ea579c9eeb526b08d6fac8550018c2d65745b0e
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-11 10:08:09 +0530

    UC-2 : Validating LastName

--- pattern.sh
+++ pattern.sh
@@ -13,3 +13,13 @@ else
    echo "FirstName is Incorrect"
 fi
 
+#UC-2 validating Last Name
+
+pattern="^[[:upper:]]{1,}[[:lower:]]{2,}$"
+read -p "Enter Last_name to check " LastName
+if [[ $LastName =~ $pattern ]]
+then
+   echo "LastName is Correct"
+else
+   echo "LastName is Incorrect"
+fi
